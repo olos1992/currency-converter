6d467921b2df7c25f95d4551eceae4f57c9bf8bf
Aleksander Skula <aleksander.skula@gmail.com>
2022-10-28 18:44:10 +0200

    functions code improved

--- js/script.js
+++ js/script.js
@@ -6,32 +6,37 @@
         const currencyIn = document.querySelector(".js-currencyIn");
         const currencyOut = document.querySelector(".js-currencyOut");
 
-        if (currencyIn.value === "PLN" && currencyOut.value === "PLN") {
-            return amountIn.value
-        } else if (currencyIn.value === "PLN" && currencyOut.value === "EUR") {
-            return (amountIn.value * 0.207).toFixed(2)
-        } else if (currencyIn.value === "PLN" && currencyOut.value === "USD") {
-            return (amountIn.value * 0.204).toFixed(2)
-        } else if (currencyIn.value === "EUR" && currencyOut.value === "PLN") {
-            return (amountIn.value * 4.81).toFixed(2)
-        } else if (currencyIn.value === "EUR" && currencyOut.value === "USD") {
-            return (amountIn.value * 0.99).toFixed(2)
-        } else if (currencyIn.value === "EUR" && currencyOut.value === "EUR") {
-            return amountIn.value
-        } else if (currencyIn.value === "USD" && currencyOut.value === "PLN") {
-            return (amountIn.value * 4.89).toFixed(2)
-        } else if (currencyIn.value === "USD" && currencyOut.value === "EUR") {
-            return (amountIn.value * 1.01).toFixed(2)
-        } else if (currencyIn.value === "USD" && currencyOut.value === "USD") {
-            return amountIn.value
+        switch (currencyIn.value + currencyOut.value) {
+            case "PLNPLN":
+            case "EUREUR":
+            case "USDUSD":
+                return (amountIn.value * 1);
+
+            case "PLNEUR":
+                return (amountIn.value * 0.207);
+
+            case "PLNUSD":
+                return (amountIn.value * 0.204);
+
+            case "EURPLN":
+                return (amountIn.value * 4.81);
+
+            case "EURUSD":
+                return (amountIn.value * 0.99);
+
+            case "USDPLN":
+                return (amountIn.value * 4.89);
+
+            case "USDEUR":
+                return (amountIn.value * 1.01);
         }
     }
 
-    const updateResult = () => {
+    const updateResultSubmit = () => {
 
         const amountOut = document.querySelector(".js-amountOut");
 
-        amountOut.value = calculateSubmit();
+        amountOut.value = (calculateSubmit()).toFixed(2);
 
     }
 
@@ -41,74 +46,62 @@
 
     }
 
+    const calculateInput = () => {
+
+        const amountInInput = document.querySelector(".js-amountInInput");
+        const currencyInInput = document.querySelector(".js-currencyInInput");
+        const currencyOutInput = document.querySelector(".js-currencyOutInput");
+
+        switch (currencyInInput.value + currencyOutInput.value) {
+            case "PLNPLN":
+            case "EUREUR":
+            case "USDUSD":
+                return (amountInInput.value * 1);
+
+            case "PLNEUR":
+                return (amountInInput.value * 0.207);
+
+            case "PLNUSD":
+                return (amountInInput.value * 0.204);
+
+            case "EURPLN":
+                return (amountInInput.value * 4.81);
+
+            case "EURUSD":
+                return (amountInInput.value * 0.99);
+
+            case "USDPLN":
+                return (amountInInput.value * 4.89);
+
+            case "USDEUR":
+                return (amountInInput.value * 1.01);
+        }
+
+    }
+
+    const updateResultInput = () => {
+
+        const amountOutInput = document.querySelector(".js-amountOutInput");
+
+        amountOutInput.value = (calculateInput()).toFixed(2);
+
+    }
+
     const init = () => {
 
         const formElementSubmit = document.querySelector(".js-formS");
 
-        formElementSubmit.addEventListener("submit", onFormSubmit);
-        formElementSubmit.addEventListener("submit", calculateSubmit);
-        formElementSubmit.addEventListener("submit", updateResult);
+        formElementSubmit.addEventListener("submit", () => {
+            calculateSubmit();
+            updateResultSubmit();
+            onFormSubmit(event);
+        });
 
+        const formElementInput = document.querySelector(".js-formI");
+
+        formElementInput.addEventListener("input", updateResultInput);
     }
- 
-        init();
 
-    };
+    init();
 
-    
-
-    {
-
-        const calculateInput = () => {
-
-            const amountInInput = document.querySelector(".js-amountInInput");
-
-            const currencyInVal = document.querySelector(".js-currencyInInput");
-            const currencyOutVal = document.querySelector(".js-currencyOutInput");
-
-            switch (currencyInVal.value + currencyOutVal.value) {
-                case "PLNPLN":
-                case "EUREUR":
-                case "USDUSD":
-                    return amountInInput.value;
-
-                case "PLNEUR":
-                    return (amountInInput.value * 0.207).toFixed(2);
-
-                case "PLNUSD":
-                    return (amountInInput.value * 0.204).toFixed(2);
-
-                case "EURPLN":
-                    return (amountInInput.value * 4.81).toFixed(2);
-
-                case "EURUSD":
-                    return (amountInInput.value * 0.99).toFixed(2);
-
-                case "USDPLN":
-                    return (amountInInput.value * 4.89).toFixed(2);
-
-                case "USDEUR":
-                    return (amountInInput.value * 1.01).toFixed(2);
-            }
-
-        }
-
-        const updateResult = () => {
-
-            const amountOutInput = document.querySelector(".js-amountOutInput");
-
-            amountOutInput.value = calculateInput ();
-
-        }
-
-        const init = () => {
-
-            const formElementInput = document.querySelector(".js-formI");
-
-            formElementInput.addEventListener("input", calculateInput);
-            formElementInput.addEventListener("input", updateResult);
-        }
-
-        init();
-
-    }
+};
